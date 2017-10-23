4dc77ca06367082594ecb22a12c035c1b615257b
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-10-22 18:38:16 -0700

    Finish kth largest

--- Project 5/chris_kim/lib/bst_practical.rb
+++ Project 5/chris_kim/lib/bst_practical.rb
@@ -1,3 +1,7 @@
-
+require_relative 'binary_search_tree'
 def kth_largest(tree_node, k)
+  bst = BinarySearchTree.new
+  sorted = bst.in_order_traversal(tree_node)
+  value = sorted[sorted.length - k]
+  bst.find(value, tree_node)
 end
