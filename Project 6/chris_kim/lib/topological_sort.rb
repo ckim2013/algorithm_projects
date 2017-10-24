02dba95b128591b43ec76067398c9a373e455f1e
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-10-23 21:33:08 -0700

    Pass topological sort spec for sorts the vertices

--- Project 6/chris_kim/lib/topological_sort.rb
+++ Project 6/chris_kim/lib/topological_sort.rb
@@ -1,7 +1,44 @@
 require_relative 'graph'
-
+require 'byebug'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  # byebug
+  sorted = []
 
+  top_queue = []
+
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      top_queue << vertex
+    end
+  end
+
+  until top_queue.empty?
+    current_node = top_queue.pop
+    sorted << current_node
+
+    until current_node.out_edges.empty?
+      edge = current_node.out_edges[0]
+      neighbor = edge.to_vertex
+      edge.destroy!
+
+      if neighbor.in_edges.empty?
+        top_queue.push(neighbor)
+      end
+
+    end
+    #
+    # current_node.out_edges.each do |edge|
+    #   # Need to figure out how to loop while deleting
+    #   neighbor = edge.to_vertex
+    #   edge.destroy!
+    #
+    #   if neighbor.in_edges.empty?
+    #     top_queue.push(neighbor)
+    #   end
+    # end
+  end
+
+  sorted
 end
