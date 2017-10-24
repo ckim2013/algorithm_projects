5a7d69f2c2f11a8463587564d01d0c8d07691684
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-10-23 19:00:21 -0700

    Finish initialize for vertex and edges

---
+++ Project 6/chris_kim/lib/graph.rb
@@ -0,0 +1,24 @@
+class Vertex
+  attr_reader :value
+  attr_accessor :in_edges, :out_edges
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
+  end
+end
+
+class Edge
+  attr_accessor :from_vertex, :to_vertex, :cost
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    to_vertex.in_edges.push(self)
+    from_vertex.out_edges.push(self)
+  end
+
+  def destroy!
+
+  end
+end
